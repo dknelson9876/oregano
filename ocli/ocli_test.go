61f1969c130334e024b50f8072c5b795c9fa0379
dknelson9876 <dknelson9876@gmail.com>
2024-02-13 21:35:03 -0700

    Add additional tests for transaction parsing

--- ocli/ocli_test.go
+++ ocli/ocli_test.go
@@ -28,3 +28,65 @@ func TestNewTrFullPositional(t *testing.T) {
 	}
 
 }
+
+func TestNewTrPartialPositional(t *testing.T) {
+	inputs := [][]string{
+		{"chase", "mcdonalds", "21.45", "3/18/2022", "fast food"},
+		{"chase", "mcdonalds", "21.45", "3/18/2022"},
+		{"chase", "mcdonalds", "21.45"},
+	}
+	expected := []omoney.Transaction{
+		omoney.NewTransaction(
+			time.Date(2022, time.March, 18, 0, 0, 0, 0, time.Local),
+			21.45,
+			"chase",
+			"mcdonalds", "fast food", "",
+		),
+		omoney.NewTransaction(
+			time.Date(2022, time.March, 18, 0, 0, 0, 0, time.Local),
+			21.45,
+			"chase",
+			"mcdonalds", "", "",
+		),
+		omoney.NewTransaction(
+			time.Now(),
+			21.45,
+			"chase",
+			"mcdonalds", "", "",
+		),
+	}
+	for i, input := range inputs {
+		tr := CreateManualTransaction(input)
+		if !expected[i].LooseEquals(tr) {
+			t.Fatalf(`Transaction("chase mcdonalds 21.45 3/18/2022 'fast food' 'big mac'")::partial`+
+				"\ngot: %+v"+
+				"\nneed %+v",
+				tr, expected[i])
+		}
+	}
+}
+
+func TestNewTrMixedPositionalAndFlag(t *testing.T) {
+	inputs := [][]string{
+		{"chase", "mcdonalds", "21.45", "3/18/2022", "-c", "fast food", "-d", "big mac"},
+		{"chase", "mcdonalds", "21.45", "3/18/2022", "-d", "big mac", "-c", "fast food"},
+		{"chase", "mcdonalds", "21.45", "-d", "big mac", "-c", "fast food", "-t", "3/18/2022"},
+	}
+	expected := omoney.NewTransaction(
+		time.Date(2022, time.March, 18, 0, 0, 0, 0, time.Local),
+		21.45,
+		"chase",
+		"mcdonalds",
+		"fast food",
+		"big mac",
+	)
+	for _, input := range inputs {
+		tr := CreateManualTransaction(input)
+		if !expected.LooseEquals(tr) {
+			t.Fatalf(`Transaction("chase mcdonalds 21.45 3/18/2022 'fast food' 'big mac'")::partial`+
+				"\ngot: %+v"+
+				"\nneed %+v",
+				tr, expected)
+		}
+	}
+}
